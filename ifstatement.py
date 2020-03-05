7f74704f344ffa182e6eb8b2eb9f1b1bcb1f560f
rajuprogrammer <61703715+rajuprogrammer@users.noreply.github.com>
2020-03-05 20:36:30 +0545

    Add files via upload

---
+++ ifstatement.py
@@ -0,0 +1,36 @@
+# x = 10
+# y = 20
+# z = 50
+
+# if x > y: 
+#     if x > z:
+#         print('x')
+#     else: 
+#         print('z')
+# else: 
+#     if y > z:
+#         print('y')
+#     else: 
+#         print('z')
+
+# p = 150000
+# t = 2.5
+# r = 2.5
+
+# i = p*t*r/100
+        
+# print(f'simple intrest is {i}')
+
+# x = i*100/t*r
+# print(f'principle is {x}')
+        
+math = 96
+science = 84
+english = 80
+nepali = 77
+account = 88
+tp = math+science+english+nepali+account/500*100
+
+print(f'total percentage is {tp}')
+
+
