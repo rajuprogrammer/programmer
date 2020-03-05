92509cbb0f175057cbb9f5fa074260fd0a06a69b
rajuprogrammer <61703715+rajuprogrammer@users.noreply.github.com>
2020-03-05 20:35:30 +0545

    Add files via upload

---
+++ number.py
@@ -0,0 +1,24 @@
+x = 12j
+print(type(x))
+
+x = 234.546743453758
+print(round(x, 2))
+
+print('{0:.3f}' .format(x))
+
+x = 1
+print(bin(x))
+
+
+x = 10
+y = 10j
+print(complex(2,10))
+
+name = 'hello ram'
+print(name[:])
+print(name[:2])
+print(name[2:])
+print(name[:-3])
+
+user = ['ram','123','sita',['hari']]
+print(user[3][0])
