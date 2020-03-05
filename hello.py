bb8f4099fdfe84c4cd541371e2d6a13a36c2dd37
rajuprogrammer <61703715+rajuprogrammer@users.noreply.github.com>
2020-03-05 20:37:28 +0545

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,25 @@
+print ("hello python")
+
+name = "sophia"
+print(name)
+
+if 5 > 2:
+    print('five is gater than 2')
+
+
+x = "5"
+y = "hello python"
+print(x)
+print(y)
+
+print('mother\'s')
+
+name = input('Enter any name: ')
+print(name)
+
+
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second numher: '))
+print(num1 + num2)
+print(type(num1))
+
