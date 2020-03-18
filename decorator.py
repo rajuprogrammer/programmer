7d7d086d2f974bfdae02edde09cae3bc3bf08505
rajuprogrammer <61703715+rajuprogrammer@users.noreply.github.com>
2020-03-18 18:26:29 +0545

    Add files via upload

---
+++ decorator.py
@@ -0,0 +1,45 @@
+# from functools import wraps
+
+# def test(anyfunction):
+#     @wraps(anyfunction)
+#     def inner():
+#         """hello inner function"""
+#         return "test"
+
+#     return inner
+
+
+# @test
+# def get():
+#     """hello get function"""
+#     return "success"
+
+# print(get.__doc__)
+
+
+# import calculator
+
+# print(calculator.add(20,30))
+
+
+# from calculator import add
+# print(add(20, 20))
+
+
+
+# print('-----------python decorator-------------')
+# def func():
+#     print("first function")
+#     def func1():
+#         print("first child function")
+#     def func2():
+#         print("second child function")
+#     func1()
+#     func2()
+# func()
+
+def name():
+    print("my name is raju")
+name()
+
+
