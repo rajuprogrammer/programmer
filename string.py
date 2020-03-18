373fbe0c2f6d2c508bc339ce7b343a90712634d1
rajuprogrammer <61703715+rajuprogrammer@users.noreply.github.com>
2020-03-18 18:30:02 +0545

    Add files via upload

---
+++ string.py
@@ -0,0 +1,22 @@
+# message = 'we are learning python'
+# print(dir(message))
+
+# username = 'hello admin'
+# x = username.replace('admin','ram')
+# print(x)
+
+# message = 'hellpyto python we are learning python'
+# print(len(message))
+
+message = 'hello python we are learning python'
+# print(message.find('we'))
+# print(message.center(100))
+# print(message.title())
+# print(message.capatilize())
+# print(message.endswith('hello'))
+# print(message.endswith('python'))
+# print(message.encode())
+# x = message.split(',')
+# print(x)
+x = message.split()
+print(x)
