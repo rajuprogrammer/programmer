d085c2ef1c236702e52599cb4b6f147edfe77f37
rajuprogrammer <61703715+rajuprogrammer@users.noreply.github.com>
2020-03-18 18:28:35 +0545

    Add files via upload

---
+++ list.py
@@ -0,0 +1,31 @@
+data = ['ram','sita','123']
+# data.insert(1,'binita')
+# print(data)
+
+# data[1] = 'hari'
+# print(data)
+
+users = ['gita','sophia','madan']
+# print(data + users)
+
+# data.append(users)
+# print(data)
+
+# data.extend(users)
+# print(data)
+
+# users.remove('gita')
+# print(users)
+
+# total = len(users)
+# x = 0
+# while x < total:
+#     print(users[x])
+#     x += 1
+
+
+# print(__file_)
+
+# things to remember in home to practice
+# study file handling....
+# the python tutorial...
